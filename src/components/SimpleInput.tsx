65168000127e7109191cd560403887ad20a3eae0
MarioAvilaJunior <juniormavila@hotmail.com>
2022-07-31 18:40:26 -0300

    Adding validation of the input also when it is touched

--- src/components/SimpleInput.tsx
+++ src/components/SimpleInput.tsx
@@ -1,12 +1,45 @@
+import React, { useState } from "react";
+
 const SimpleInput = () => {
+  const [name, setName] = useState<string>("");
+  const [nameWasTouched, setNameWasTouched] = useState<boolean>(false);
+
+  const nameIsValid = name.trim().length > 0;
+  const nameWasInvalidated = !nameIsValid && nameWasTouched;
+
+  const nameChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const nameBlurHander = () => {
+    setNameWasTouched(true);
+  };
+
+  const formSubmissionHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    setName("");
+    setNameWasTouched(false);
+  };
+
+  const errorClass = `form-control${nameWasInvalidated ? " invalid" : ""}`;
+
   return (
-    <form>
-      <div className="form-control">
+    <form onSubmit={formSubmissionHandler}>
+      <div className={errorClass}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={nameChangeHandler}
+          onBlur={nameBlurHander}
+        />
+        {nameWasInvalidated && (
+          <p className="error-text">Name must not be empty</p>
+        )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </div>
     </form>
   );
